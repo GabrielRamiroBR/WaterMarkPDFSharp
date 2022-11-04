fec561e91419b4ffed38079a9a3f42baf03d8150
GabrielRamiroBR <gabrielramirom@gmail.com>
2022-11-04 14:18:06 -0300

    Modification to add watermark in every page of the pdf document

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 using System;
+using System.ComponentModel;
 
 namespace PdfSharp
 {
     class Program
     {
-        public static void LoadCenteredTiltedWatermark(PdfPage page, XGraphics graphics, XImage image)
+        //Draw tilted watermark on the specified graphics
+        private static void DrawCenteredTiltedWatermark(PdfPage page, XGraphics graphics, XImage image)
         {
             // Define a rotation transformation at the center of the page.
             graphics.TranslateTransform(page.Width / 2, page.Height / 2);
@@ -24,24 +26,28 @@ namespace PdfSharp
             XSolidBrush brush = new XSolidBrush(XColor.FromArgb(200, 255, 255, 255));
             graphics.DrawRectangle(brush, 0, 0, page.Width, page.Height);
         }
+        //Add an image based watermark to every page in the PDF document
+        private static void AddWatermarkInPdfDocument(PdfPages pages, XImage image)
+        {
+            foreach(PdfPage page in pages)
+            {
+                var graphics = PdfSharp.Drawing.XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Prepend);
+                DrawCenteredTiltedWatermark(page, graphics, image);
+            }
+        }
 
         static void Main (string[] args)
         {
             //Creates a pdf doc, a page and the Xgraphics to draw on the page
-            using PdfDocument doc = PdfReader.Open("C:\\Users\\gabmes61\\source\\repos\\TestesPDF\\TestesPDF\\TEST.pdf");
-            var page = doc.Pages[0];
-            var graphics = PdfSharp.Drawing.XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Prepend);
+            using PdfDocument doc = PdfReader.Open("C:\\Users\\gabmes61\\source\\repos\\TestesPDF\\TestesPDF\\Test.pdf");
+            var pages = doc.Pages;
             XImage image = XImage.FromFile("C:\\Users\\gabmes61\\source\\repos\\TestesPDF\\TestesPDF\\ComosLogo.png");
 
-            //Load watermark on page
-            LoadCenteredTiltedWatermark(page, graphics, image);
+            AddWatermarkInPdfDocument(pages, image);
 
             //Line of code to avoid a NotSupportedException to be thrown when saviung the doc
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-            doc.Save("testFileWatermark.pdf");
-
-            //modificar para ler carregar várias páginas
- 
+            doc.Save("TestWatermark.pdf");
         }
     }
 }
